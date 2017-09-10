6bbb9c0d1394db10cff8311d48493a60c9fbb058
Emmanuel Fleurine <emmanuelfleurine@gmail.com>
2017-09-10 17:24:23 -0400

    Create the inital working library

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": "airbnb-base/legacy",
+  "parserOptions": {
+    "ecmaVersion": 8
+  },
+  "rules": {
+    "semi": 'off',
+    "indent": ["error", 2],
+    "no-console": ["error", { allow: ["warn", "error"] }] ,
+   },
+   
+};
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Mini-Api
+
+A flexible router for building json api on top of Zeit's [micro](https://github.com/zeit/micro)
+
+* Not yet read for production
